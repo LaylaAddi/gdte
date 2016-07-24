6258e2d9c4b4d53a34227a008be4e179a44145fb
steve chesnowitz <steve@chesnowitz.com>
2016-07-24 03:33:44 +0000

    gem "therubyracer"

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,4 +6,8 @@ config.fog_credentials = {
   :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
 }
 config.fog_directory     =  ENV['AWS_BUCKET_NAME']
+
+compute = Fog::Compute.new(:provider => 'AWS', :aws_access_key_id => AWS_ACCESS_KEY_ID, :aws_secret_access_key => AWS_SECRET_ACCESS_KEY)
+
+storage = Fog::Storage.new(:provider => 'AWS', :aws_access_key_id => AWS_ACCESS_KEY_ID, :aws_secret_access_key => AWS_SECRET_ACCESS_KEY)
 end
