fed9c9b2834b7af02613447ff45797dd98bab0d1
steve chesnowitz <steve@chesnowitz.com>
2016-07-24 04:21:56 +0000

    gem "therubyracer"

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,7 +3,8 @@ config.fog_credentials = {
   # Configuration for Amazon S3
   :provider              => 'AWS',
   :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-  :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+  :region                => ENV['AWS_REGION']
 }
   config.fog_public     = false  
   config.fog_directory  = ENV['AWS_BUCKET_NAME']
