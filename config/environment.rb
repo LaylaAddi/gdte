197fbe5f955d32c7ebcc6b0896609699a38cd16c
steve chesnowitz <steve@chesnowitz.com>
2016-07-24 04:06:39 +0000

    gem "therubyracer"

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Rails.application.configure do
-  config.aws_access_key_id     = ENV['AWS_ACCESS_KEY_ID']
-  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-end
