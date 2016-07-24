3f47303df0554f1855947293d909bd3a66d1f9a8
steve chesnowitz <steve@chesnowitz.com>
2016-07-24 03:54:46 +0000

    gem "therubyracer"

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,6 @@ require_relative 'application'
 Rails.application.initialize!
 
 Rails.application.configure do
-  config.s3_access_key_id     = ENV['S3_ACCESS_KEY_ID']
-  config.s3_secret_access_key = ENV['S3_SECRET_ACCESS_KEY']
+  config.s3_access_key_id     = ENV['AWS_ACCESS_KEY_ID']
+  config.s3_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,8 +12,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => Rails.configuration.s3_access_key_id,
-    :aws_secret_access_key  => Rails.configuration.s3_secret_access_key,
+    :aws_access_key_id      => Rails.configuration.aws_access_key_id,
+    :aws_secret_access_key  => Rails.configuration.aws_secret_access_key,
   }
   config.fog_directory     =  ENV['AWS_BUCKET_NAME']
 end
