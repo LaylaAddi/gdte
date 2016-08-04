37e2ca2719af52714d28334b25b2c79154f5deea
steve chesnowitz <steve@chesnowitz.com>
2016-08-04 04:28:03 +0000

    allow_iframe_requests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, except: [:index ]
   before_action :authenticate_driver!, except: [:index ]  
   before_action :disable_navigation
+  before_filter :allow_iframe_requests
   
   def index 
     if current_driver
